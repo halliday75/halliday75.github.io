1a79d531650f9121b9f91e7f1cbf914adac81d02
halliday75 <111073756+halliday75@users.noreply.github.com>
2023-06-01 11:40:26 +0100

    Update blog.js

--- testSite/blog.js
+++ testSite/blog.js
@@ -1,6 +1,6 @@
 import data from 'https://halliday75.github.io/testSite/blog.json' assert { type: 'json' };
 console.log(data);
-var size = Object.keys(data).length;
+var size = Object.keys(data.blogPosts).length;
 for (var i = 0; i < size; i++) {
   document.getElementById('posts').innerHTML += "<h3>" + data.blogPosts[i].title + "</h3><p>" + data.blogPosts[i].content + "</p><p>" + data.blogPosts[i].author + " - " + data.blogPosts[i].date + "</p>";
 }
