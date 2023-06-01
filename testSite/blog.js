659cdc4cd3809057704a06b66b23439a49ee2bc6
halliday75 <111073756+halliday75@users.noreply.github.com>
2023-06-01 10:52:54 +0100

    Update blog.js

--- testSite/blog.js
+++ testSite/blog.js
@@ -1,6 +1,4 @@
 import data from 'blog.json' assert { type: 'json' };
 let blogPosts = data;
 console.log(data);
-for (var i = 0; i < 10; i++) {
-  document.write("<p>" + blogPosts[i].title + "</p>");
-}
+document.write(JSON.stringify(blogPosts));
