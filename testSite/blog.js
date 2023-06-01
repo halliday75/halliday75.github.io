37c263d9c2ce58f6fab613f0effc12f642ae3ab5
halliday75 <111073756+halliday75@users.noreply.github.com>
2023-06-01 10:54:52 +0100

    Update blog.js

--- testSite/blog.js
+++ testSite/blog.js
@@ -1,4 +1,4 @@
-import data from 'blog.json' assert { type: 'json' };
+import data from '/blog.json' assert { type: 'json' };
 let blogPosts = data;
 console.log(data);
 document.write(JSON.stringify(blogPosts));
