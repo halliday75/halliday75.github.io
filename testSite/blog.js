03fc6f88b5c5dec02814ef277b16b25d45726731
halliday75 <111073756+halliday75@users.noreply.github.com>
2023-07-06 16:06:02 +0100

    Update blog.js

--- testSite/blog.js
+++ testSite/blog.js
@@ -1,3 +1,19 @@
+function startContext(w, o) {
+      try {
+        var h = document.getElementsByTagName('head')[0];
+        var s = document.createElement('script');
+        s.type = 'text/javascript';
+        s.async = 1;
+        s.crossOrigin = 'anonymous';
+        s.src = '//d2c7xlmseob604.cloudfront.net/tracker.min.js';
+        s.onload = function () {
+          w.SmartlingContextTracker.init({ orgId: o });
+        };
+        h.insertBefore(s, h.firstChild);
+      } catch (ex) {
+      }
+    }
+startContext(window, 'oiWelnQtGz-UjmA4j40ROg');
 import data from './blog.json' assert { type: 'json' };
 var size = Object.keys(data.blogPosts).length;
 for (var i = 0; i < size; i++) {
