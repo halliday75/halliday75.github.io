d1fb397893545eb1c785edf636fde383ceae2af5
halliday75 <111073756+halliday75@users.noreply.github.com>
2024-03-27 23:20:10 +0000

    Add files via upload

---
+++ splunk/script.js
@@ -0,0 +1,17 @@
+function changeIframeSrc(newSrc) {
+    document.getElementById('dynamicIframe').src = newSrc;
+};
+document.getElementById('searchInput').addEventListener('input', function (event) {
+    const searchTerm = event.target.value.toLowerCase();
+    const listItems = document.querySelectorAll('#itemList li');
+
+    listItems.forEach(function (item) {
+        const itemText = item.textContent.toLowerCase();
+
+        if (itemText.includes(searchTerm)) {
+            item.style.display = 'list-item';
+        } else {
+            item.style.display = 'none';
+        }
+    });
+});
