8ee7aa6a4becbf63947d28d9f9c478b210656191
halliday75 <111073756+halliday75@users.noreply.github.com>
2025-11-10 10:26:57 +0000

    Update testing.js

--- testing.js
+++ testing.js
@@ -10,14 +10,14 @@ sl_tr_html_end   = sl_translate;
 sl_tr_json_start = sl_translate;
 sl_tr_json_end   = sl_translate;
 
+// ► Smartling translation block ◄
+sl_tr_start();
 
 const me = {
-  // ► Smartling translation block ◄
-  sl_tr_tart();
   enData: {
     LO_TITLE: "Log Out Alert",
     LO_MESSAGE: "You have been logged out due to inactivity",
     LO_CONTINUE: "Go to Login Screen"
   }
-  sl_tr_end();
 };
+sl_tr_end();
