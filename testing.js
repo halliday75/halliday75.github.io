c3b9b8559189cdf36711f3ab6cbe79d59ba3c0ef
halliday75 <111073756+halliday75@users.noreply.github.com>
2025-11-10 10:24:21 +0000

    Fix typo in LO_TITLE for translation block

--- testing.js
+++ testing.js
@@ -1,7 +1,23 @@
+// Smartling directive function declarations (required to survive minification)
+function sl_translate() {}
+
+sl_tr_start     = sl_translate;
+sl_tr_end       = sl_translate;
+sl_notr_start   = sl_translate;
+sl_notr_end     = sl_translate;
+sl_tr_html_start = sl_translate;
+sl_tr_html_end   = sl_translate;
+sl_tr_json_start = sl_translate;
+sl_tr_json_end   = sl_translate;
+
+
 const me = {
+  // ► Smartling translation block ◄
+  sl_tr_tart();
   enData: {
-    LO_TITLE: "Log Out Altert",
+    LO_TITLE: "Log Out Alert",
     LO_MESSAGE: "You have been logged out due to inactivity",
     LO_CONTINUE: "Go to Login Screen"
   }
-}
+  sl_tr_end();
+};
